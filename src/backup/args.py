a7460d632fa9e2c552e40aeffa838c284a80d090
Dillon Evans <dillonje@uw.edu>
2024-02-18 20:41:21 -0800

    Commented args.py and added the Arguments data class

--- src/backup/args.py
+++ src/backup/args.py
@@ -1,24 +1,44 @@
-from argparse import ArgumentParser, ArgumentError, ArgumentTypeError
-from os import path
+import os
+from argparse import ArgumentParser, ArgumentTypeError, Namespace
 from typing import Tuple
 
 
-def parse_arguments():
+class Arguments:
+    """Data class for storing command line arguments."""
+
+    def __init__(self, args: Namespace) -> None:
+        """
+        Initializes the object from the Namespace object.
+        :param args: The Namespace from ArgumentParser.parse_args().
+        """
+        self.source_dir: str = args.source
+        self.bucket_name: str = args.destination[0]
+        self.bucket_dir: str = args.destination[1]
+
+
+def parse_arguments() -> Arguments:
+    """
+    Parses command line arguments.
+    Exits the program if the arguments are invalid.
+    :return: The parsed arguments.
+    """
     parser = ArgumentParser(description='Makes a backup to AWS.')
     parser.add_argument('source', metavar='directory', type=directory, help='directory to backup')
-    parser.add_argument('destination', metavar='bucket::directory', type=bucket_and_directory, help='bucket and directory to backup to')
-    return parser.parse_args()
+    parser.add_argument('destination', metavar='bucket::directory', type=bucket_and_directory,
+                        help='bucket and directory to backup to')
+    return Arguments(parser.parse_args())
 
 
 def directory(raw_path: str) -> str:
-    if path.isdir(raw_path):
-        return raw_path
-    else:
-        raise ArgumentTypeError(f'no such file or directory: {raw_path}')
+    """Meant to be used a type converter for ArgumentParser.add_argument()."""
+    if not os.path.isdir(raw_path):
+        raise ArgumentTypeError('no such directory')
+    return raw_path
 
 
 def bucket_and_directory(name: str) -> Tuple[str, str]:
+    """Meant to be used a type converter for ArgumentParser.add_argument()."""
     elements = name.split('::')
     if len(elements) != 2:
-        raise ArgumentError()
+        raise ArgumentTypeError('missing bucket directory')
     return elements[0], elements[1]
